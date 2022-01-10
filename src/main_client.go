2a71e44e78699ac40171136da9053466f4c5e101
Khib0o <louisteys59310@gmail.com>
2022-01-10 02:25:56 +0100

    Code basique fini

--- src/main_client.go
+++ src/main_client.go
@@ -6,9 +6,8 @@ import (
 	"net"
 	"os"
 	"strconv"
-	"bufio"
+	"bufio"	
     "strings"
-    "time"
 )
 
 func check(e error) {
@@ -21,18 +20,44 @@ func main() {
 
 	//graph_string := "Graph1\nA,B,C,D\n{0,2,5,3}\n{2,0,0,0}\n{5,0,0,4}\n{3,0,4,0}$"
 
-	graph_string := "Graph1\nA,B,C,D,E,F\n{0,0,1,0,0,0}\n{0,0,0,0,0,2}\n{1,0,0,0,0,5}\n{0,0,0,0,3,0}\n{0,0,0,3,0,1}\n{0,2,5,0,1,0}\n$"
+	graph_string := "TestTD2\nA,B,C,D,E,F\n{0,2,100,0,0,0}\n{2,0,10,60,0,0}\n{100,10,0,0,3,2}}\n{0,60,0,0,4,0}\n{0,0,3,4,0,2}\n{0,0,2,0,1,0}\n$"
 
 	port := 10000
 
-	if len(os.Args) != 3 {
-		fmt.Printf("Usage: go run client.go <portnumber> <graph_string>\n")
+	if len(os.Args) != 4 {
+		fmt.Printf("Usage: go run client.go <portnumber> <file> <graph_number>\n")
 		os.Exit(1)
 	} else {
 		fmt.Printf("#DEBUG ARGS Port Number : %s\n", os.Args[1])
 		portNumber, err := strconv.Atoi(os.Args[1])
 		check(err)
 		port = portNumber
+		file, err := os.Open(os.Args[2])
+		check(err)
+
+		reader := bufio.NewReader(file)
+		compteur := 0
+		nbG, err := strconv.Atoi(os.Args[3])
+		check(err)
+		for {
+			test, err := reader.ReadString('$')
+			check(err)
+			compteur= compteur + 1
+    		if (compteur == nbG){
+				graph_string = test
+				graph_string = strings.Replace(graph_string,"\\n","\n",-1)
+				fmt.Printf(graph_string)
+				fmt.Printf("\n")
+				//fmt.Print("TestTD2\nA,B,C,D,E,F\n{0,2,100,0,0,0}\n{2,0,10,60,0,0}\n{100,10,0,0,3,2}}\n{0,60,0,0,4,0}\n{0,0,3,4,0,2}\n{0,0,2,0,1,0}\n$")
+				break
+			}
+		}
+
+		graph_string = strings.TrimPrefix(graph_string, "\n")
+
+		file.Close()
+
+		//graph_string = "TestTD2\nA,B,C,D,E,F\n{0,2,100,0,0,0}\n{2,0,10,60,0,0}\n{100,10,0,0,3,2}}\n{0,60,0,0,4,0}\n{0,0,3,4,0,2}\n{0,0,2,0,1,0}\n$"
 		//graph_string = os.Args[2]
 	}
 
@@ -50,8 +75,6 @@ func main() {
         reader := bufio.NewReader(conn)
 		fmt.Printf("#DEBUG MAIN connected\n")
 
-		fmt.Sprintf(graph_string)
-
         io.WriteString(conn, fmt.Sprintf(graph_string))
 
 		fmt.Printf("#DEBUG MAIN message envoyé\n")
@@ -63,7 +86,6 @@ func main() {
         }
         resultString = strings.TrimSuffix(resultString, "$")
         fmt.Printf("#DEBUG server replied : -------------\n%s\n-------------\n", resultString)
-        time.Sleep(1000 * time.Millisecond)
 
 	}
 
